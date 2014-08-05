b5a32c6d3654cafd6d1d485644b855f6c81c596f
Genesis Guerrero <genesisdaft@gmail.com>
2014-08-05 16:51:57 -0500

    CRUD de guias

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::resource('naviera', 'NavieraController');
 Route::resource('proveedores', 'ProveedoresController');
+Route::resource('guias', 'GuiasController');
 
 Route::get('/', function()
 {
@@ -25,9 +15,4 @@ Route::get('/registro', function()
 	//return View::make('hello');
     return View::make('registro');
 });
- Route::get('/prueba', function(){
-     $compa = new Container();
-     $compa->numero_container = 'jasd';
-     $compa->save();
-     return 'datos guardados correctamente';
- });
+
