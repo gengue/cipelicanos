1b386bfc572d6fa0776b2a84de5de856e2309522
jochechavez <joche.echavezm@gmail.com>
2014-08-06 20:19:56 -0500

    nada

--- app/config/database.php
+++ app/config/database.php
@@ -68,7 +68,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'cipelicanos',
 			'username'  => 'root',
-			'password'  => 'admin',
+			'password'  => '1234',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
