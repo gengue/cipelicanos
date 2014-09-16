cb4cd43d1e247c37b58382b33df92b41b0db4652
Andresvz <andresvz91@gmail.com>
2014-09-16 17:32:03 -0500

    lo mio

--- app/config/database.php
+++ app/config/database.php
@@ -26,13 +26,9 @@ return array(
 	|
 	*/
 
-<<<<<<< HEAD
+
 	'default' => 'cipelicanos_mySQL',
      //'default' => 'cipelicanos2_mySQL',
-=======
-	//'default' => 'cipelicanos_mySQL',
-     'default' => 'cipelicanos2_mySQL',
->>>>>>> d0da0a9f0e7db1ce30857a5502355f38d845a9f0
 
 	/*
 	|--------------------------------------------------------------------------
