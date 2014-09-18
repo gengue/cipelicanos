2b4a40b802c03fb146604535bc11c29af750405f
Genesis Guerrero <genesisdaft@gmail.com>
2014-09-17 23:34:52 -0500

    Agregado cambiar contraseña en modulo de cliente y administrador, arreglado editar perfil

--- app/config/database.php
+++ app/config/database.php
@@ -27,8 +27,8 @@ return array(
 	*/
 
 
-	'default' => 'cipelicanos_mySQL',
-    //'default' => 'cipelicanos2_mySQL',
+	//'default' => 'cipelicanos_mySQL',
+    'default' => 'cipelicanos2_mySQL',
 
 	/*
 	|--------------------------------------------------------------------------
